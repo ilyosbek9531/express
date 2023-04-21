fb6309b7e843e3863af771b71523b01aaed9555d
00014039wiut <shohruxabdusaidov@gmail.com>
2023-04-22 01:56:08 +0500

    get request && post request && env configuration && express.json() midleware && nodemon configuration for quick run the project

--- src/app.js
+++ src/app.js
@@ -1,22 +1,32 @@
 const express = require("express");
+// used for to make readable env file
+require("dotenv").config();
 
 const app = express();
 
-const PORT = 4000;
+// taking port from env file
+const PORT = process.env.PORT;
 
+// used for convert to json file between client side and server side
+app.use(express.json());
+
+// basic get request
 app.get("/", (req, res) => {
   res.send([
     {
       id: 1,
       name: "ilyosbek",
     },
-    {
-      id: 2,
-      name: "alisher",
-    },
   ]);
 });
 
+// basic post request
+app.post("/", (req, res) => {
+  console.log("1", req.body);
+  res.send(201);
+});
+
+// listening port
 app.listen(PORT, () => {
   console.log("Running express server on port 4000");
 });
