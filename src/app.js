f2471079892c890cbf9508182a57a15ae12d1871
00014039wiut <shohruxabdusaidov@gmail.com>
2023-04-22 02:44:15 +0500

    midleware

--- src/app.js
+++ src/app.js
@@ -9,20 +9,43 @@ const PORT = process.env.PORT;
 
 // used for convert to json file between client side and server side
 app.use(express.json());
+// It parses incoming requests with URL-encoded payloads and is based on a body parser.
+app.use(express.urlencoded());
+
+app.use((req, res, next) => {
+  console.log(`${req.method}:${req.url}`);
+  next();
+});
+
+const users = [];
 
 // basic get request
-app.get("/", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      name: "ilyosbek",
-    },
-  ]);
-});
+app.get(
+  "/",
+  // midleware
+  (req, res, next) => {
+    console.log("Before handling request");
+    next();
+  },
+  // midleware
+  (req, res, next) => {
+    res.send(users);
+    next();
+  },
+  // midleware
+  (req, res, next) => {
+    console.log("Finished Executing GET request");
+    next();
+  },
+  // midleware
+  () => {
+    console.log("The end");
+  }
+);
 
 // basic post request
 app.post("/", (req, res) => {
-  console.log("1", req.body);
+  users.push(req.body);
   res.send(201);
 });
 
