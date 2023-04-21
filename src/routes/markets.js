1601c81df4ce95fcd89e82acc8edfd215872ad14
ilyosbek9531 <iqboljonsheraliyev11@gmail.com>
2023-04-22 04:23:46 +0500

    query parametrs

--- src/routes/markets.js
+++ src/routes/markets.js
@@ -6,15 +6,58 @@ const markets = [
   {
     location: "toshkent",
     name: "havas",
+    miles: 0.7,
   },
   {
     location: "fargona",
     name: "macro",
+    miles: 1.5,
+  },
+  {
+    location: "namangan",
+    name: "havas",
+    miles: 2.3,
+  },
+  {
+    location: "buxoro",
+    name: "macro",
+    miles: 5.1,
+  },
+  {
+    location: "navoiy",
+    name: "havas",
+    miles: 9.0,
+  },
+  {
+    location: "andijon",
+    name: "macro",
+    miles: 18.5,
+  },
+  {
+    location: "sirdaryo",
+    name: "havas",
+    miles: 19.3,
+  },
+  {
+    location: "samarqand",
+    name: "macro",
+    miles: 4.1,
   },
 ];
 
 router.get("/", (req, res) => {
-  res.send(markets);
+  const { miles, sort } = req.query;
+  const parsedMiles = parseFloat(miles);
+  if (!isNaN(parsedMiles)) {
+    const filterByMiles = markets.filter((item) =>
+      sort == "ASC"
+        ? item.miles > parsedMiles
+        : sort == "DSC"
+        ? item.miles < parsedMiles
+        : item.miles == parsedMiles
+    );
+    res.send(filterByMiles);
+  } else res.send(markets);
 });
 
 router.get("/:name", (req, res) => {
